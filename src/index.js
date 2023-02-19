8a4db74a7f84affae58cde47ba756589ee35b0eb
keyminy <imanaifs@gmail.com>
2023-02-19 21:26:56 +0900

    common.js형태도 가능

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-//import es6A,{value,hello as hi} from './lib_es6';
-/* 모듈 전부 import하기 */
-import * as M from './lib_es6';
-
-console.log(M);
-
-const {
-  a,
-  fnt
-} = M.default;
-
-console.log(a);//10
-fnt();//실무코딩
-M.print('메시지');
-//hello('실무코딩');
-//hi('실무코딩'); //안녕하십니까? 실무코딩님!
+const com = require('./lib_es6');
+console.clear();
+console.log(com,com.value);
+com.print('메시지');
