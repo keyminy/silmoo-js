1061553cb5aca0c671f7ee2f5d93464d9deda498
keyminy <imanaifs@gmail.com>
2023-02-19 21:11:25 +0900

    ECMAScript module import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-//const comA = require('./lib_com')
-const {a,fnt} = require('./lib_com')
+import es6A,{value,hello as hi} from './lib_es6';
 
-//const {a,fnt} = com;
+const {
+  a,
+  fnt
+} = es6A;
 
-console.log(a);
-fnt();
+console.log(a);//10
+fnt();//실무코딩
+//hello('실무코딩');
+hi('실무코딩'); //안녕하십니까? 실무코딩님!
---
+++ src/lib_es6.js
@@ -0,0 +1,8 @@
+export const value = 50;
+
+export const hello = name => console.log(`안녕하십니까? ${name}님!`);
+
+export default {
+  a:10,
+  fnt: () => console.log('실무코딩')
+}
